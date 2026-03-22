6541a87b7e2af97dac9967874e934011bf529057
david19rp <davidramirezperez@outlook.com>
2026-03-22 04:17:25 -0600

    testing

---
+++ src/test/java/cr/david/casoevaluado_1/ParticipanteTest.java
@@ -0,0 +1,13 @@
+package cr.david.casoevaluado_1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParticipanteTest {
+
+    @Test
+    public void testSumaBasica() {
+        int resultado = 2 + 3;
+        assertEquals(5, resultado);
+    }
+}
